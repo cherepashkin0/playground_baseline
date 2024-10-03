a94146475e6b049618d37370267c100c88ba04c2
cherepashkin0 <cherepashkin0@gmail.com>
2024-10-03 06:39:12 +0200

    wandb works

--- main.py
+++ main.py
@@ -3,22 +3,72 @@ from myimports import *
 from training import *
 from mypp import *
 from gc import collect
-from config import *
 from pathlib import Path
+import wandb
+import random
+import optuna
+import yaml
 
-cv_selector = \
-{
- "RKF"   : RKF(n_splits = CFG.n_splits, n_repeats= CFG.n_repeats, random_state= CFG.state),
- "RSKF"  : RSKF(n_splits = CFG.n_splits, n_repeats= CFG.n_repeats, random_state= CFG.state),
- "SKF"   : SKF(n_splits = CFG.n_splits, shuffle = True, random_state= CFG.state),
- "KF"    : KFold(n_splits = CFG.n_splits, shuffle = True, random_state= CFG.state),
- "GKF"   : GKF(n_splits = CFG.n_splits)
-}
-# collect()
-pp = Preprocessor()
-pp.DoPreprocessing()
-cat_mdl_cols = \
-    list(pp.test.select_dtypes([np.int8, np.int16, np.int32, np.int64]).columns) + pp.cat_cols
+import hydra
+from omegaconf import DictConfig, OmegaConf
+
+class CFG:
+    pass
+
+@hydra.main(version_base=None, config_path=".", config_name="config")
+def config_init(cfg: DictConfig) -> None:
+    # Print the configuration in YAML format (optional, for debugging)
+    # print(OmegaConf.to_yaml(cfg))
+
+    # Load parameters from 'general' section of the config to class attributes
+    for key, value in cfg['general'].items():
+        setattr(CFG, key, value)
+
+# with open('config.yaml', 'r') as file:
+    # config = yaml.safe_load(file)
+
+@hydra.main(version_base=None, config_path=".", config_name="config")
+def wandb_init(cfg: DictConfig):
+    # print(OmegaConf.to_yaml(cfg))  # For debugging, print the config in YAML format
+    cfg_dict = OmegaConf.to_container(cfg, resolve=True)  # Convert DictConfig to a dictionary
+
+    # Initialize wandb with the converted dictionary
+    wandb.init(
+        project=cfg_dict['wandb_params']['project'],
+        config=cfg_dict,  # Now passing the standard dictionary
+        mode=cfg_dict['wandb_params']['mode']
+    )
+
+@hydra.main(version_base=None, config_path=".", config_name="config")
+def model_training_params(cfg: DictConfig):
+    cfg_dict = OmegaConf.to_container(cfg, resolve=True)  # Convert DictConfig to a dictionary
+    models_config = cfg_dict['models']
+
+    # Use the loaded config to initialize models
+    Mdl_Master = {
+        'LGBM1C': LGBMC(**models_config['LGBM1C']),
+        'LGBM2C': LGBMC(**models_config['LGBM2C']),
+        'XGB1C': XGBC(**models_config['XGB1C']),
+        'CB1C': CBC(**models_config['CB1C']),
+    }
+
+    # Modify device parameter dynamically based on CFG.gpu_switch
+    if CFG.gpu_switch == "ON":
+        Mdl_Master['LGBM1C'].set_params(device='gpu')
+        Mdl_Master['LGBM2C'].set_params(device='gpu')
+        Mdl_Master['XGB1C'].set_params(device='cuda')
+    else:
+        Mdl_Master['LGBM1C'].set_params(device='cpu')
+        Mdl_Master['LGBM2C'].set_params(device='cpu')
+        Mdl_Master['XGB1C'].set_params(device='cpu')
+    # print([64]*20)
+    # print(type(Mdl_Master), Mdl_Master)
+    # Return the model configuration dictionary
+    return Mdl_Master
+
+
+
+    # wandb.log({"acc": acc, "loss": loss})
 
 def data_transform():
 
@@ -26,8 +76,6 @@ def data_transform():
     Xtrain = pp.train.drop([CFG.target, CFG.grouper], axis= 1, errors = "ignore")
     PrintColor(f"---> Shapes = {Xtrain.shape} {ytrain.shape} {pp.test.shape}")
 
-    # cat_mdl_cols = \
-    # list(pp.test.select_dtypes([np.int8, np.int16, np.int32, np.int64]).columns) + pp.cat_cols
 
     Xtrain = MakeFtre(Xtrain, cat_mdl_cols)
     Xtest  = MakeFtre(pp.test, cat_mdl_cols)
@@ -64,95 +112,30 @@ def MakeFtre(X: pd.DataFrame, cat_cols: list):
     df = X.copy()
     df["loantoincome"] = (df["loan_amnt"] / df["person_income"]) - df["loan_percent_income"]
 
+    df['age_income_interaction'] = df['person_age'] * df['person_income']
+    df['income_loan_amnt_interaction'] = df['person_income'] * df['loan_amnt']
+    df['loan_amnt_int_rate_interaction'] = df['loan_amnt'] * df['loan_int_rate']
+    df['int_rate_to_income_ratio'] = df['loan_int_rate'] / df['person_income']
+    df['emp_length_to_age_ratio'] = df['person_emp_length'] / df['person_age']
+
+
     df[cat_cols] = df[cat_cols].astype("category")
     return df
 
 
-def model_training():
-    try:
-        l = MyLogger()
-        l.init(logging_lbl = "lightgbm_custom")
-        lgb.register_logger(l)
-    except:
-        pass
-
-    # Initializing model parameters
-    Mdl_Master = \
-    {
-     f'LGBM1C' : LGBMC(**{"objective"           : "binary",
-                          "metrics"             : "auc",
-                          'device'              : "gpu" if CFG.gpu_switch == "ON" else "cpu",
-                          'learning_rate'       : 0.0325,
-                          'n_estimators'        : 5_000,
-                          'max_depth'           : 7,
-                          'num_leaves'          : 25,
-                          'min_data_in_leaf'    : 20,
-                          'feature_fraction'    : 0.70,
-                          'bagging_fraction'    : 0.88,
-                          'bagging_freq'        : 6,
-                          'lambda_l1'           : 0.001,
-                          'lambda_l2'           : 0.1,
-                          'verbosity'           : -1,
-                          'random_state'        : CFG.state,
-                         }
-                      ),
-
-     f'LGBM2C' : LGBMC(**{"objective"           : "binary",
-                          "metrics"             : "auc",
-                          'device'              : "gpu" if CFG.gpu_switch == "ON" else "cpu",
-                          'learning_rate'       : 0.035,
-                          'data_sample_strategy': 'goss',
-                          'n_estimators'        : 5_000,
-                          'max_depth'           : 7,
-                          'num_leaves'          : 30,
-                          'min_data_in_leaf'    : 30,
-                          'feature_fraction'    : 0.60,
-                          'colsample_bytree'    : 0.65,
-                          'lambda_l1'           : 0.001,
-                          'lambda_l2'           : 1.25,
-                          'verbosity'           : -1,
-                          'random_state'        : CFG.state,
-                         }
-                      ),
+def barplot_scores(scores_all):
+    data = []
+    for model, scores in scores_all.items():
+        data.append([model, -np.log10(1-scores[0]), -np.log10(1-scores[1])])
+    table = wandb.Table(data=data, columns=["Model", "OOF Mean", "Train Mean"])
+    wandb.log({"Model Performance": table})
+    # Use wandb built-in bar plot functionality
+    wandb.log({
+        "OOF Mean Values": wandb.plot.bar(table, "Model", "OOF Mean", title="OOF Mean Values"),
+        "Train Mean Values": wandb.plot.bar(table, "Model", "Train Mean", title="Train Mean Values")
+    })
 
 
-     f'XGB1C' : XGBC(**{  "objective"             : "binary:logistic",
-                          "eval_metric"           : "auc",
-                          'device'                : "cuda" if CFG.gpu_switch == "ON" else "cpu",
-                          'learning_rate'         : 0.03,
-                          'n_estimators'          : 5_000,
-                          'max_depth'             : 7,
-                          'colsample_bytree'      : 0.75,
-                          'colsample_bynode'      : 0.85,
-                          'colsample_bylevel'     : 0.45,
-                          'reg_alpha'             : 0.001,
-                          'reg_lambda'            : 0.25,
-                          'verbose'               : 0,
-                          'random_state'          : CFG.state,
-                          'enable_categorical'    : True,
-                          'callbacks'             : [XGBLogging(epoch_log_interval= 0)],
-                          'early_stopping_rounds' : CFG.nbrnd_erly_stp,
-                         }
-                      ),
-
-     f'CB1C' : CBC(**{'task_type'           : "CPU",
-                      'loss_function'       : 'Logloss',
-                      'eval_metric'         : "AUC",
-                      'bagging_temperature' : 0.25,
-                      'colsample_bylevel'   : 0.40,
-                      'iterations'          : 5_000,
-                      'learning_rate'       : 0.045,
-                      'max_depth'           : 7,
-                      'l2_leaf_reg'         : 0.80,
-                      'min_data_in_leaf'    : 30,
-                      'random_strength'     : 0.25,
-                      'verbose'             : 0,
-                      'cat_features'        : cat_mdl_cols,
-                     }
-                  ),
-    }
-
-    return Mdl_Master
 
 def single_model(Mdl_Master, Xtrain, Xtest, ytrain, ygrp):
     # Initializing model outputs
@@ -165,7 +148,7 @@ def single_model(Mdl_Master, Xtrain, Xtest, ytrain, ygrp):
 
     # Model training:-
     drop_cols = ["Source", "id", "Id", "Label", CFG.target, "fold_nb"]
-
+    scores_all = {}
     for method, mymodel in tqdm(Mdl_Master.items()):
 
         PrintColor(f"\n{'=' * 20} {method.upper()} MODEL TRAINING {'=' * 20}\n")
@@ -186,7 +169,7 @@ def single_model(Mdl_Master, Xtrain, Xtest, ytrain, ygrp):
         PrintColor(f"Selected columns = {len(sel_mdl_cols) :,.0f}", color = Fore.RED)
         SelMdlCols[method] = sel_mdl_cols, cat_mdl_cols
 
-        fitted_models, oof_preds, test_preds, ftreimp, mdl_best_iter =  \
+        fitted_models, oof_preds, test_preds, ftreimp, mdl_best_iter, scores_all[method] =  \
         md.MakeOfflineModel(
             Xtrain[sel_mdl_cols].copy(),
             deepcopy(ytrain),
@@ -200,18 +183,22 @@ def single_model(Mdl_Master, Xtrain, Xtest, ytrain, ygrp):
             # ntop = 50,
         )
 
+
+
         OOF_Preds[method]    = oof_preds
         Mdl_Preds[method]    = test_preds
         FittedModels[method] = fitted_models
         FtreImp[method]      = ftreimp
 
-        del fitted_models, oof_preds, test_preds, ftreimp, sel_mdl_cols
-        print()
-        collect();
+        # del fitted_models, oof_preds, test_preds, ftreimp, sel_mdl_cols
+        # print()
+        # collect();
+    barplot_scores(scores_all)
+    return OOF_Preds, Mdl_Preds
+    # _ = utils.CleanMemory();
 
-    _ = utils.CleanMemory();
 
-def ensemble():
+def ensemble(Xtrain, ytrain, ygrp, OOF_Preds, Mdl_Preds):
     # %%time
 
     len_train  = len(Xtrain.loc[Xtrain.Source == "Competition"])
@@ -247,15 +234,32 @@ def ensemble():
         ens_preds[dev_idx] = dev_preds
 
     PrintColor(f"\n---> Mean Score = {scores:.6f}")
+    wandb.log({"logistic_ensemble": scores})
 
 
 def main():
+    wandb_init()
     Path('figs').mkdir(exist_ok=True, parents=True)
     PrintColor(f"\n---> Configuration done!\n")
     Xtrain, Xtest, ytrain, ygrp = data_transform()
-    Mdl_Master = model_training()
-    single_model(Mdl_Master, Xtrain, Xtest, ytrain, ygrp)
-    ensemble()
+    Mdl_Master = model_training_params()
+    OOF_Preds, Mdl_Preds = single_model(Mdl_Master, Xtrain, Xtest, ytrain, ygrp)
+    ensemble(Xtrain, ytrain, ygrp, OOF_Preds, Mdl_Preds)
+
 
 if __name__=="__main__":
+    config_init()
+    cv_selector = \
+    {
+     "RKF"   : RKF(n_splits = CFG.n_splits, n_repeats= CFG.n_repeats, random_state= CFG.state),
+     "RSKF"  : RSKF(n_splits = CFG.n_splits, n_repeats= CFG.n_repeats, random_state= CFG.state),
+     "SKF"   : SKF(n_splits = CFG.n_splits, shuffle = True, random_state= CFG.state),
+     "KF"    : KFold(n_splits = CFG.n_splits, shuffle = True, random_state= CFG.state),
+     "GKF"   : GKF(n_splits = CFG.n_splits)
+    }
+    # collect()
+    pp = Preprocessor()
+    pp.DoPreprocessing()
+    cat_mdl_cols = \
+        list(pp.test.select_dtypes([np.int8, np.int16, np.int32, np.int64]).columns) + pp.cat_cols
     main()
